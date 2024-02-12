a122ac00b33c46a677651a1b8142c4ff289fbe31
Alexey <a.serezhenko@inbox.ru>
2024-02-12 05:59:24 +0300

    6 commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import './App.css';
 import Header from './components/Header/Header'
 import {Route, Routes} from 'react-router-dom'
 import ProductList from './components/ProductList/ProductList'
-import Form from './components/Form/Form'
 function App() {
 
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route  index element={<ProductList />}/> 
-        <Route path={'/form'} element={<Form />} />
+        <Route  index element={<ProductList />} /> 
       </Routes>
     </div>
   );
