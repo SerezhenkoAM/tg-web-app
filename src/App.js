c7428f003c6fb9d348109b465e6ff169d2f91820
Alexey <a.serezhenko@inbox.ru>
2024-02-12 04:42:37 +0300

    6 commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route  index element={<ProductList />}/>
-        <Route path={'/form'} element={<Form/>} />
+        <Route  index element={<Form/>}/> 
+        <Route path={'/form'} element={<ProductList />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+//<ProductList />
