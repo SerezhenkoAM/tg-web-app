c5297d6be17fe691225e926c61dcaa830188886f
Alexey <a.serezhenko@inbox.ru>
2024-02-12 06:13:48 +0300

    6 commit

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const ProductItem = ({product,onAdd}) => {
   const addProduct = () => {
-    onAdd(product)
+    onAdd(product.title)
   }
   return (
     <div>
       <div>{product.title}</div>
       <div>{product.price}</div>
-      <button onClick={addProduct(product.title)}>Добавить в корзину</button>
+      <button onClick={addProduct()}>Добавить в корзину</button>
     </div>
   )
 }
