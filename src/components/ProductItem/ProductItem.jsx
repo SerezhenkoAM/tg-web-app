18e9739e1430c07616823061a2d64d3c9f9cd7e1
Alexey <a.serezhenko@inbox.ru>
2024-02-12 05:56:20 +0300

    6 commit

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProductItem = (product,onAdd) => {
+  const addProduct = () => {
+    onAdd(product)
+  }
+  return (
+    <div>
+      <div>{product.title}</div>
+      <div>{product.price}</div>
+      <button onClick={addProduct}>Добавить в корзину</button>
+    </div>
+  )
+}
+
+export default ProductItem
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProductItem from '../ProductItem/ProductItem'
 
 const ProductList = () => {
+  const products = [
+    {title: 'Ряженка', price: 2000},
+    {title: 'Сливки', price: 6000},
+    {title: 'Хлеб', price: 5000},
+    {title: 'Мясо', price: 3000},
+    {title: 'Рис', price: 1000}
+  ]
+  const [addedItems,setAddedItems] = useState([])
+  const tg = window.Telegram.WebApp
+  const onAdd = (product) => {
+    setAddedItems(product.id)
+    if (addedItems === 0) {
+      tg.MainButton.hide()
+    } else {
+      tg.MainButton.show()
+      tg.MainButtons.setParams({
+        text: `Купить ${addedItems}`
+      })
+    }
+  }
   return (
     <div>
       ProductList
+      {
+        products.map(item => {
+          <ProductItem 
+              product={item}
+              onAdd={onAdd}
+          />
+        })
+      }
     </div>
   )
 } 
