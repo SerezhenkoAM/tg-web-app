cd10cd5342f0485759e583c7b0e30e3290c944b2
Alexey <a.serezhenko@inbox.ru>
2024-02-19 22:04:10 +0300

    c

--- src/components/Header/UserInfo/UserInfo.jsx
+++ src/components/Header/UserInfo/UserInfo.jsx
@@ -3,19 +3,27 @@ import styles from './UserInfo.module.css'
 const UserInfo = () => {
   const tg = window.Telegram.WebApp
   useEffect(() => {
-    console.log(tg.initData)
-    async function getUserInfo() {
-      const user = await tg.getUserProfilePhotos({userId: tg.initDataUnsafe?.user?.id, offset: 0, limit: 1});
-      const photo = user.photos[0][0].fileId;
-      console.log(user)
-      return photo;
+    fetch('https://api.telegram.org/file/bot6599850488:AAE7qcVF-evvXSdycqH9sOjMAeYtNX1Wijc/photos/file_0.jpg')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-    console.log(getUserInfo)
+    return response.blob();
+  })
+  .then(blob => {
+    const url = URL.createObjectURL(blob);
+    const img = new Image();
+    img.src = url;
+    document.body.appendChild(img); // добавляем изображение на страницу
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
   })
   return (
     <div className={styles.wrapp}>
       {tg.initDataUnsafe?.photo_url}
-        <img src={tg.initDataUnsafe?.photo_url} alt="Фотография" className={styles.photo_url}/>
+        <img src={tg.initDataUnsafe?.photo_url == null } alt="Фотография" className={styles.photo_url}/>
         <div className="">
           <p>Добрый день,</p>
           <p>{tg.initDataUnsafe?.user?.first_name}</p>
