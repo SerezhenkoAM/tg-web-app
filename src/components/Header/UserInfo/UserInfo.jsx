0e0dcbd4746109e4229849e87132ab290fc6784b
Alexey <a.serezhenko@inbox.ru>
2024-02-19 19:15:19 +0300

    c

--- src/components/Header/UserInfo/UserInfo.jsx
+++ src/components/Header/UserInfo/UserInfo.jsx
@@ -4,7 +4,7 @@ const UserInfo = () => {
   const tg = window.Telegram.WebApp
   return (
     <div className={styles.wrapp}>
-        <img src={tg.initDataUnsafe?.photo_url} alt="Фотография пользователя" className={styles.photo_url}/>
+        <img src={tg.initDataUnsafe?.photo_url} alt="Фотография" className={styles.photo_url}/>
         <div className="">
           <p>Добрый день,</p>
           <p>{tg.initDataUnsafe?.user?.first_name}</p>
