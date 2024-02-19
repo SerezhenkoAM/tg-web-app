22142ef576c69f3a249b88b9eb52053dbb03b18f
Alexey <a.serezhenko@inbox.ru>
2024-02-19 21:27:13 +0300

    c

--- src/components/Header/UserInfo/UserInfo.jsx
+++ src/components/Header/UserInfo/UserInfo.jsx
@@ -3,7 +3,13 @@ import styles from './UserInfo.module.css'
 const UserInfo = () => {
   const tg = window.Telegram.WebApp
   useEffect(() => {
-    console.log(tg)
+    console.log(tg.initData)
+    async function getUserInfo() {
+      const user = await telegram.getUserProfilePhotos({userId: '@username', offset: 0, limit: 1});
+      const photo = user.photos[0][0].fileId;
+      return photo;
+    }
+    console.log(getUserInfo)
   })
   return (
     <div className={styles.wrapp}>
